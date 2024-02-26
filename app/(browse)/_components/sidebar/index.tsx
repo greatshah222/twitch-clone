b9038b2b68a33c086eb59fcf7667f906b573a73d
Bishal Shah <great.shah222@gmail.com>
2024-02-26 23:02:37 +0200

    recommended list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,12 +1,28 @@
+import { getRecommended } from "@/lib/recommended";
+
+import { Recommended, RecommendedSkeleton } from "./recommended";
 import { Toggle } from "./toggle";
 import { Wrapper } from "./wrapper";
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+	const recommended = await getRecommended();
 	return (
 		// THIS WRAPPER IS A SERVER COMPONENT ->WE USED CHILDREN METHOD FOR THIS TO MAKE IT WORK
 		<Wrapper>
 			{/* THIS TOGGLE IS A SERVER COMPONENT */}
 			<Toggle />
+
+			<div className="spa-y-4 pt-4 lg:pt-0">
+				<Recommended data={recommended} />
+			</div>
 		</Wrapper>
+	);
+};
+
+export const SidebarSkeleton = () => {
+	return (
+		<aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+			<RecommendedSkeleton />
+		</aside>
 	);
 };
---
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { User } from "@prisma/client";
+
+import { useSidebar } from "@/store/use-sidebar";
+import { UserItem, UserItemSkeleton } from "./user-item";
+
+interface RecommendedProps {
+	data: User[];
+}
+
+export const Recommended = ({ data }: RecommendedProps) => {
+	const { collapsed } = useSidebar((state) => state);
+
+	const showLabel = !collapsed && data?.length > 0;
+
+	return (
+		<div>
+			{showLabel && (
+				<div className="pl-6 mb-4">
+					<p className="text-sm text-muted-foreground">Recommended</p>
+				</div>
+			)}
+
+			<ul className="space-y-2 px-2">
+				{data?.map((el) => (
+					<UserItem key={el.id} username={el.username} imageUrl={el.imageUrl} isLive={true} />
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export const RecommendedSkeleton = () => {
+	return (
+		<ul className="px-2">
+			{[...Array(3)].map((_, i) => (
+				<UserItemSkeleton key={i} />
+			))}
+		</ul>
+	);
+};
